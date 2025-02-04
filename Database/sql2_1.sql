6b246eeae78561edc3da142e5086b6c1c6737aef
nguyenluongk2k4 <nguyenluongk2k4@gmail.com>
2025-02-04 08:44:09 +0700

    sửa db

---
+++ Database/sql2_1.sql
@@ -0,0 +1,8 @@
+-- Insert data into Doctors
+INSERT INTO Doctors (account_id, doctor_name, experience_years, specialization_id, profile_image, rating, gender, dob, address) VALUES
+(2, 'Lee Min Ho', 10, 1, 'leeminho.jpg', 4.8, 'Male', '1987-06-22', 'Seoul, South Korea'),
+(3, 'Song Hye Kyo', 8, 2, 'songhyekyo.jpg', 4.6, 'Female', '1981-11-22', 'Seoul, South Korea'),
+(4, 'Kim Soo Hyun', 12, 3, 'kimsoohyun.jpg', 4.9, 'Male', '1988-02-16', 'Seoul, South Korea'),
+(5, 'Park Shin Hye', 7, 4, 'parkshinhye.jpg', 4.5, 'Female', '1990-02-18', 'Gwangju, South Korea'),
+(6, 'Ji Chang Wook', 9, 5, 'jichangwook.jpg', 4.7, 'Male', '1987-07-05', 'Seoul, South Korea');
+
