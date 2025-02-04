a9cc42baae169122d0b9111b365f7d245da48b30
susu1601 <susurikitami@gmail.com>
2025-02-05 01:24:09 +0700

    list doctor by specialization Id

--- SWP391/src/java/dal/DoctorsDAO.java
+++ SWP391/src/java/dal/DoctorsDAO.java
@@ -72,6 +72,42 @@ public class DoctorsDAO extends DBContext {
         return list;
     }
 
+    public List<Doctors> getDoctorsBySpecializationId(String sid) {
+        List<Doctors> list = new ArrayList<>();
+        String sql = "SELECT d.doctor_id, d.doctor_name, d.experience_years, d.profile_image, d.rating, d.gender, d.dob, d.address, sp.specialization_id, sp.specialization_name, sp.specialization_status \n"
+                + "FROM [dbo].[Doctors] d JOIN [dbo].[Specialization] sp "
+                + "ON d.specialization_id = sp.specialization_id "
+                + "where d.specialization_id = ?";
+
+        try {
+            PreparedStatement st = connection.prepareStatement(sql);
+            st.setString(1, sid);
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                Doctors doctor = new Doctors();
+                doctor.setDoctor_id(rs.getInt("doctor_id"));
+                doctor.setDoctor_name(rs.getString("doctor_name"));
+                doctor.setExperience_years(rs.getInt("experience_years"));
+                doctor.setProfile_image(rs.getString("profile_image"));
+                doctor.setRating(rs.getDouble("rating"));
+                doctor.setGender(rs.getString("gender"));
+                doctor.setDob(rs.getString("dob"));
+                doctor.setAddress(rs.getString("address"));
+
+                Specialization specialization = new Specialization();
+                specialization.setSpecialization_id(rs.getInt("specialization_id"));
+                specialization.setSpecialization_name(rs.getString("specialization_name"));
+                specialization.setSpecialization_status(rs.getString("specialization_status"));
+                doctor.setSpecialization(specialization);
+
+                list.add(doctor);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
     public static void main(String[] args) {
         DoctorsDAO dao = new DoctorsDAO();
         List<Doctors> list = dao.getAllDoctors();
@@ -80,9 +116,11 @@ public class DoctorsDAO extends DBContext {
 //            System.out.println(doctors);
 //
 //        }
-        for (Specialization sp : listsp) {
-            System.out.println(sp);
-        }
+//        for (Specialization sp : listsp) {
+//            System.out.println(sp);
+//        }
+//          List<Doctors> listbySid = dao.getDoctorsBySpecializationId("2");
+//          System.out.println(listbySid);
     }
 
 }
