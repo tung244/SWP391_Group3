b58f2d675d0981cebc632e0db0102681dcf1ba97
susu1601 <susurikitami@gmail.com>
2025-02-11 17:31:15 +0700

    update

--- Database/sql2_1.sql
+++ Database/sql2_1.sql
@@ -1,7 +1,7 @@
 
 -- Insert data into Doctors
-USE EyeCare
-INSERT INTO Doctors (account_id, doctor_name, experience_years, specialization_id, profile_image, rating, gender, dob, address,, doctor_status) VALUES
+
+INSERT INTO Doctors (account_id, doctor_name, experience_years, specialization_id, profile_image, rating, gender, dob, address, doctor_status) VALUES
 (2, 'Lee Min Ho', 10, 1, 'https://res.cloudinary.com/djmftornv/image/upload/v1739089349/doctor/lxilek68buesdfztcww3.jpg', 4.8, 'Male', '1987-06-22', 'Seoul, South Korea','Active'),
 (3, 'Song Hye Kyo', 8, 2, 'https://res.cloudinary.com/djmftornv/image/upload/v1739089348/doctor/e6cmqgkqartwvvxo45s0.jpg', 4.6, 'Female', '1981-11-22', 'Seoul, South Korea','Active'),
 (4, 'Kim Soo Hyun', 12, 3, 'https://res.cloudinary.com/djmftornv/image/upload/v1739089348/doctor/h1en31pbtigykshv0tf7.jpg', 4.9, 'Male', '1988-02-16', 'Seoul, South Korea','Active'),
@@ -36,12 +36,5 @@ INSERT INTO Degree_Doctor (doctor_id, degree_id) VALUES
 
 
 
--- Insert data into Doctors
-USE EyeCare
-INSERT INTO Doctors (account_id, doctor_name, experience_years, specialization_id, profile_image, rating, gender, dob, address) VALUES
-(2, 'Lee Min Ho', 10, 1, 'https://res.cloudinary.com/djmftornv/image/upload/v1739089349/doctor/lxilek68buesdfztcww3.jpg', 4.8, 'Male', '1987-06-22', 'Seoul, South Korea'),
-(3, 'Song Hye Kyo', 8, 2, 'https://res.cloudinary.com/djmftornv/image/upload/v1739089348/doctor/e6cmqgkqartwvvxo45s0.jpg', 4.6, 'Female', '1981-11-22', 'Seoul, South Korea'),
-(4, 'Kim Soo Hyun', 12, 3, 'https://res.cloudinary.com/djmftornv/image/upload/v1739089348/doctor/h1en31pbtigykshv0tf7.jpg', 4.9, 'Male', '1988-02-16', 'Seoul, South Korea'),
-(5, 'Park Shin Hye', 7, 4, 'https://res.cloudinary.com/djmftornv/image/upload/v1739089347/doctor/endxfdr01bzwk4sjpbfa.jpg', 4.5, 'Female', '1990-02-18', 'Gwangju, South Korea'),
-(6, 'Ji Chang Wook', 9, 5, 'https://res.cloudinary.com/djmftornv/image/upload/v1739089474/doctor/bwsuvnyhcaha1xfhfau1.jpg', 4.7, 'Male', '1987-07-05', 'Seoul, South Korea');
+
 
