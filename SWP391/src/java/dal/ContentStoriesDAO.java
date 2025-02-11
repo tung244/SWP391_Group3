726286a362c179559158e9077de25dbb90b28240
nguyenluongk2k4 <nguyenluongk2k4@gmail.com>
2025-02-11 09:57:34 +0700

    update

--- SWP391/src/java/dal/ContentStoriesDAO.java
+++ SWP391/src/java/dal/ContentStoriesDAO.java
@@ -22,8 +22,8 @@ public class ContentStoriesDAO extends DBContext{
             ResultSet rs = st.executeQuery();
             while (rs.next()) {
                 ContentStories e = new ContentStories(rs.getString(1),
-                        rs.getString(2),
-                        rs.getString(3));
+                        rs.getString(3),
+                        rs.getString(2));
                 list.add(e);
             }
 
