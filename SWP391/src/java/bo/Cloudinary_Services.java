3076c671566a0b2296f0ffb042054f239be33aad
Khoa2305 <dikhoadiem@gmail.com>
2025-02-18 16:54:15 +0700

    update

--- SWP391/src/java/bo/Cloudinary_Services.java
+++ SWP391/src/java/bo/Cloudinary_Services.java
@@ -6,7 +6,8 @@ package bo;
 
 import com.cloudinary.utils.ObjectUtils;
 import static consts.CloudinaryConfig.cloudinary;
-import jakarta.servlet.http.Part;
+import jakarta.mail.Part;
+//import jakarta.servlet.http.Part;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,36 +16,36 @@ import java.util.Map;
 
 
 public class Cloudinary_Services {
-    public static String uploadImage(Part part) throws IOException{
-
-        File tempFile = File.createTempFile("upload-", ".jpg");  // tạo name file VD: upload-724109055963885517.jpg(na ná v)
-        try (InputStream fileContent = part.getInputStream()) {
-            Files.copy(fileContent, tempFile.toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
-        }
-        Map uploadResult;
-        try {
-            uploadResult = cloudinary.uploader().upload(tempFile, ObjectUtils.asMap(
-                "folder", "test"  // này là thư mục lưu ảnh, có j thì kêu t tạo rồi add tên vô
-            ));
-        } finally {
-            tempFile.delete(); // Xóa file tạm thời nãy tạo đi
-        }
-        
-        System.out.println(uploadResult.toString());
-        // Lấy URL ảnh ra dùng cái này lưu db này
-        String imageUrl = (String) uploadResult.get("secure_url");
-        
-        return imageUrl;
-    }
-    
-    public static void deleteImage(String public_id_img){
-        
-        try{
-            Map result = cloudinary.uploader().destroy(public_id_img, ObjectUtils.asMap());
-        }
-        catch(Exception e){
-            e.printStackTrace();
-        }
-        
-    }
+//    public static String uploadImage(Part part) throws IOException{
+//
+//        File tempFile = File.createTempFile("upload-", ".jpg");  // tạo name file VD: upload-724109055963885517.jpg(na ná v)
+//        try (InputStream fileContent = part.getInputStream()) {
+//            Files.copy(fileContent, tempFile.toPath(), java.nio.file.StandardCopyOption.REPLACE_EXISTING);
+//        }
+//        Map uploadResult;
+//        try {
+//            uploadResult = cloudinary.uploader().upload(tempFile, ObjectUtils.asMap(
+//                "folder", "test"  // này là thư mục lưu ảnh, có j thì kêu t tạo rồi add tên vô
+//            ));
+//        } finally {
+//            tempFile.delete(); // Xóa file tạm thời nãy tạo đi
+//        }
+//        
+//        System.out.println(uploadResult.toString());
+//        // Lấy URL ảnh ra dùng cái này lưu db này
+//        String imageUrl = (String) uploadResult.get("secure_url");
+//        
+//        return imageUrl;
+//    }
+//    
+//    public static void deleteImage(String public_id_img){
+//        
+//        try{
+//            Map result = cloudinary.uploader().destroy(public_id_img, ObjectUtils.asMap());
+//        }
+//        catch(Exception e){
+//            e.printStackTrace();
+//        }
+//        
+//    }
 }
